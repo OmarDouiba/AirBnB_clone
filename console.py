5491ba7bb59c8a27fc7de2b91f076a4d0f55d8da
DouibaOmar <douibaomar40@gmail.com>
2023-11-11 14:18:50 +0100

    Done

--- console.py
+++ console.py
@@ -17,14 +17,14 @@ class HBNBCommand(Cmd):
         waiting for the commands
         class_name: Class Attribute list
         classes in Airbnb Project
-    
+
     Methods:
         do_create(self, args): Create a new
         instance of a class.
         do_show(self, args): Show information
         about an instance.
         do_destroy(self, args): Delete an instance.
-        do_all(self, args): Print all instances. 
+        do_all(self, args): Print all instances.
         emptyline(self): Print all instances.
         do_quit(self, args): Quit the command
         interpreter.
@@ -32,19 +32,17 @@ class HBNBCommand(Cmd):
     """
     prompt = "(hbnb) "
     class_name = {
-        "BaseModel": BaseModel,
-        
-    }
+        "BaseModel": BaseModel}
     # count = 0
 
     def do_create(self, args):
         """Create a new instance of a class."""
         args_list = args.split()
         # print("--> {}".format(args_list))
-        
+
         if not args_list:
             print("** class name missing **")
-        
+
         elif args_list[0] in self.class_name.keys():
             new_obj = self.class_name[args_list[0]]()
             new_obj.save()
@@ -52,10 +50,10 @@ class HBNBCommand(Cmd):
             # self.count += 1
             # print(self.count)
             # print(new_obj)
-        
+
         else:
             print("** class doesn't exist **")
-        
+
         # if not args_list:
         #     print("** class name missing **")
         # else:
@@ -67,34 +65,34 @@ class HBNBCommand(Cmd):
         #                 obj.save()
         #             elif arg not in self.class_name.keys():
         #                 print("** class doesn't exist **")
-    
+
     def do_show(self, args):
         """Show information about an instance."""
         args_list = shlex.split(args)
 
         if len(args_list) < 1:
             print("** class name missing **")
-        
+
         elif args_list[0] not in self.class_name.keys():
             print("** class doesn't exist **")
-        
+
         elif len(args_list) < 2:
             print("** instance id missing **")
-        
+
         else:
             id_list = []
             key_list = []
-            
+
             for key in models.storage.all().keys():
                 key_list = key.split('.')[1]
                 id_list.append(key_list)
-            
+
             if args_list[1] in id_list:
                 key = ".".join([args_list[0], args_list[1]])
                 # key = f"{args_list[0]}.{args_list[1]}"
                 obj = models.storage._FileStorage__objects
                 print(obj[key])
-            
+
             else:
                 return print("** no instance found **")
 
@@ -104,10 +102,10 @@ class HBNBCommand(Cmd):
 
         if len(args_list) < 1:
             print("** class name missing **")
-        
+
         elif len(args_list) < 2:
             print("** instance id missing **")
-        
+
         else:
             input_key = f"{args_list[0]}.{args_list[1]}"
             obj = models.storage._FileStorage__objects
@@ -127,13 +125,13 @@ class HBNBCommand(Cmd):
         if len(args_list) == 0:
             for val in obj.values():
                 obj_list.append(str(val))
-        
+
         elif args_list[0] in self.class_name.keys():
             for key, val in obj.items():
                 key = key.split(".")[0]
                 if args_list[0] == key:
                     obj_list.append(str(val))
-        
+
         else:
             return print("** class doesn't exist **")
         print(obj_list)
@@ -154,6 +152,7 @@ class HBNBCommand(Cmd):
     def do_EOF(self, args):
         """Exit on EOF (Ctrl+D)."""
         return True
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
