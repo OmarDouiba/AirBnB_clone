ff7aded3c2e1d29f3f8f5aae17b6fc3cf0262a92
DouibaOmar <douibaomar40@gmail.com>
2023-11-11 14:11:38 +0100

    Done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,7 +19,7 @@ class FileStorage():
         all objects by <class name>.id
         class_name: Class Attribute list
         classes in Airbnb Project.
-    
+
     Methods:
         all(self): Return the dictionary __objects
         new(self, obj): Set in __objects
@@ -29,7 +29,7 @@ class FileStorage():
         reload(self): Deserialize the JSON
         file (path: __file_path) to __objects
     """
-    __file_path =  "file.json"
+    __file_path = "file.json"
     __objects = {}
     class_name = {
         'BaseModel': BaseModel}
@@ -53,18 +53,18 @@ class FileStorage():
         for key, obj in self.__objects.items():
             new_dict[key] = obj.to_dict()
 
-        with open(self.__file_path, "w", 
+        with open(self.__file_path, "w",
                   encoding="utf-8") as save_file:
             dump(new_dict, save_file)
-    
+
     def reload(self):
         """Deserialize the JSON file
         (path: __file_path) to __objects"""
         if exists(self.__file_path):
-            with open(self.__file_path, 
+            with open(self.__file_path,
                       "r") as read_file:
                 file_data = load(read_file)
-            
+
             for key, val in file_data.items():
                 dict_class = val["__class__"]
                 # dict_class, _ = key.split(".")
