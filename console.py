bb2bb85ea4be062d7cabeb57f867c6378bf8d259
DouibaOmar <douibaomar40@gmail.com>
2023-11-12 13:01:20 +0100

    Done

--- console.py
+++ console.py
@@ -200,7 +200,7 @@ class HBNBCommand(Cmd):
                         args_list[3] = int(args_list[3])
                     elif args_list[3].replace('.', '').isdigit():
                         args_list[3] = float(args_list[3])
-                    
+
                     obj_dict = obj[class_key]
                     obj_dict[args_list[2]] = args_list[3]
                     models.storage.save()
