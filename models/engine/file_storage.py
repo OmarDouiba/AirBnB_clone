1f93f34477b845d07ada05e68fcf76b7ec5cf618
DouibaOmar <douibaomar40@gmail.com>
2023-11-12 14:09:49 +0100

    Done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,9 +62,9 @@ class FileStorage():
         JSON file (path: __file_path)"""
         new_dict = {}
 
-        for key, obj in self.__objects.items():
+        for key, obj in FileStorage().__objects.items():
             new_dict[key] = obj.to_dict()
-        with open(FileStorage().__file_path, "w",
+        with open(self.__file_path, "w",
                   encoding="utf-8") as save_file:
             json.dump(new_dict, save_file)
 
